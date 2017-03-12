931d191a922e36638dd151e17900a21b7a4a7ba9
Will Sonnex <will@sonnex.name>
2017-03-12 14:55:04 +0000

    general memes

--- run.php
+++ run.php
@@ -2,14 +2,14 @@
 <body style="font-family: monospace;">
 <?php
 $src = $_REQUEST['src'];
-$filename = uniqid() . "-" . date(DateTime::ISO8601) . ".elea";
+$filename = uniqid() . ".elea";
 
 try {
 	$file = fopen("elea/" . $filename, "w");
 	fwrite($file, $src);
 	fclose($file);
 
-	$proofs = shell_exec("cd elea && ulimit -t 30 elea $filename 2>&1");
+	$proofs = shell_exec("cd elea && ulimit -t 30 ./elea $filename 2>&1");
 	echo nl2br($proofs);
 } catch (Exception $e) {
 	echo "Exception: " . $e->getMessage();
